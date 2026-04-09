e8aef7c5e3aefa9db5ea24bab4239bdfcacaba97
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2026-04-10 01:30:08 +0300

    refactor(user-accounts): SNAP-3 consolidate core module structure and update imports
    - Moved `CoreConfig` to a dedicated `config` folder within `core`.
    - Adjusted module and import paths to reflect the updated structure.

--- apps/user-accounts/src/core/core.module.ts
+++ apps/user-accounts/src/core/core.module.ts
@@ -1,5 +1,5 @@
 import { Global, Module } from '@nestjs/common';
-import { CoreConfig } from './core.config';
+import { CoreConfig } from './config/core.config';
 import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
 import { USER_ACCOUNTS_EXCHANGE } from '@snaptix/contracts';
 
