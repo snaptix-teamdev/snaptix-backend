3dbf0f713c25a1326e76f609305ac45896a644c3
defDimych <def.dimych@gmail.com>
2026-05-08 21:26:38 +0300

    feat(get-active-devices): SNAP-12 add get-active-devices endpoint

--- apps/user-accounts/src/accounts/api/security-devices.controller.ts
+++ apps/user-accounts/src/accounts/api/security-devices.controller.ts
@@ -4,12 +4,24 @@ import {
   RefreshTokenPayload,
   USER_ACCOUNTS_PATTERNS,
 } from '@snaptix/contracts';
-import { CommandBus } from '@nestjs/cqrs';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { DeactivateSessionsExcludingCurrentCommand } from '../application/commands/session-commands/deactivate-sessions-excluding-current.usecase';
+import { GetActiveDevicesResponseDto } from '@snaptix/contracts/user-accounts/get-active-devices/get-active-devices.response-dto';
+import { GetActiveDevicesQuery } from '../application/queries/get-active-devices.query';
 
 @Controller('security/devices')
 export class SecurityDevicesController {
-  constructor(private commandBus: CommandBus) {}
+  constructor(
+    private commandBus: CommandBus,
+    private queryBus: QueryBus,
+  ) {}
+
+  @MessagePattern(USER_ACCOUNTS_PATTERNS.SECURITY_DEVICES.GET_ACTIVE_DEVICES)
+  async getActiveDevices(
+    @Payload() payload: RefreshTokenPayload,
+  ): Promise<GetActiveDevicesResponseDto[]> {
+    return this.queryBus.execute(new GetActiveDevicesQuery(payload));
+  }
 
   @MessagePattern(
     USER_ACCOUNTS_PATTERNS.SECURITY_DEVICES.DEACTIVATE_ALL_EXCLUDING_CURRENT,
---
+++ apps/user-accounts/src/accounts/application/queries/get-active-devices.query.ts
@@ -0,0 +1,60 @@
+import { ISession } from '@snaptix/common';
+import { IQueryHandler, Query, QueryHandler } from '@nestjs/cqrs';
+import { TokensService } from '../services/tokens.service';
+import { SessionsQueryRepository } from '../../../infrastructure/query/sessions.query-repository';
+
+class GetActiveDevicesQueryPayload {
+  refreshToken: string;
+}
+
+class GetActiveDevicesQueryResponse implements Pick<
+  ISession,
+  'ip' | 'deviceId'
+> {
+  ip: string | null;
+  title: string;
+  lastActiveDate: string;
+  deviceId: string;
+  current: boolean;
+}
+
+export class GetActiveDevicesQuery extends Query<
+  GetActiveDevicesQueryResponse[]
+> {
+  constructor(public payload: GetActiveDevicesQueryPayload) {
+    super();
+  }
+}
+
+@QueryHandler(GetActiveDevicesQuery)
+export class GetActiveDevicesQueryHandler implements IQueryHandler<
+  GetActiveDevicesQuery,
+  GetActiveDevicesQueryResponse
+> {
+  constructor(
+    private sessionsQueryRepository: SessionsQueryRepository,
+    private tokensService: TokensService,
+  ) {}
+
+  async execute({
+    payload,
+  }: GetActiveDevicesQuery): Promise<GetActiveDevicesQueryResponse[]> {
+    const session = await this.tokensService.validateRefreshTokenOrThrow(
+      payload.refreshToken,
+    );
+
+    const sessions = await this.sessionsQueryRepository.findAllByUserId(
+      session.userId,
+    );
+
+    return sessions.map((s) => {
+      return {
+        ip: s.ip,
+        title: s.deviceName,
+        lastActiveDate: s.issuedAt.toISOString(),
+        deviceId: s.deviceId,
+        current: s.deviceId === session.deviceId,
+      };
+    });
+  }
+}
